8a4e86cf5c386c0a36d0800798ad8a27ecd04dde
Hyeonjin Kim <andrew75313@gmail.com>
2024-05-16 12:44:20 +0900

    SpringBoot의 JPA 환경 조성 : Schedule Entity 변경

--- src/main/java/com/sparta/scheduler/entity/Schedule.java
+++ src/main/java/com/sparta/scheduler/entity/Schedule.java
@@ -1,18 +1,28 @@
 package com.sparta.scheduler.entity;
 
 import com.sparta.scheduler.dto.SchedulerRequestDto;
+import com.sparta.scheduler.dto.SchedulerRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
 @NoArgsConstructor
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "username", nullable = false)
     private String username;
+    @Column(name = "username", nullable = false)
     private String password;
+    @Column(name = "contents", nullable = false, length = 500)
     private String title;
+    @Column(name = "contents", nullable = false, length = 500)
     private String contents;
 
     public Schedule(SchedulerRequestDto requestDto) {
